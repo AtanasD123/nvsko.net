707f0baf4b4128ea3e74d5155602f574829694d5
AtanasD123 <98911730+AtanasD123@users.noreply.github.com>
2025-04-28 00:20:51 -0400

    v3

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/nvsko.net/",  
+export default defineConfig(({ mode }) => ({ 
   server: {
     host: "::",
     port: 8080,
