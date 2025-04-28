2ae02b15dfe6e2f9f08a78f07e2e46a123d909a2
AtanasD123 <98911730+AtanasD123@users.noreply.github.com>
2025-04-28 01:19:01 -0400

    thesechatbotscansuckmyschwantz

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
           <ScrollToTop /> 
           <Layout> {/* <-- WRAP Routes inside Layout */}
             <Routes>
-              <Route path="/*" element={<Index />} />
+              <Route path="/*" element={<Index />} />     {/* didn't even exist */}
               <Route path="/index" element={<Index />} />  {/* used to be "/" */}
               <Route path="/library" element={<Library />} />
               <Route path="/blog" element={<Blog />} />
